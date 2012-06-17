bc4abdd9c64fbe85a38507cfc01ed9f000e28078
Ádamo Morone <adamomorone@iMac-de-Adamo-Morone.local>
2012-06-17 01:12:16 -0300

    EventDetailViewController Added

---
+++ iPhone/Real Time Life/controller/EventDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventDetailViewController.h
+//  Real Time Life
+//
+//  Created by Ádamo Morone on 17/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventDetailViewController : UIViewController
+
+@end
