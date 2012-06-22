f87c478e14bad1d7218483c391cff7527b59d087
Ádamo Morone <adamomorone@iMac-de-Adamo-Morone.local>
2012-06-22 20:07:00 -0300

    Parameters are ok in Detail Screen

--- iPhone/Real Time Life/classes/Annotation.h
+++ iPhone/Real Time Life/classes/Annotation.h
@@ -15,11 +15,19 @@
     NSInteger idUser;
     NSString* title;
     NSString* subtitle;
+    CLLocationCoordinate2D  coordinate;
+    NSInteger               idUser; //New Attribute
+    NSString*               title;
+    NSString*               subtitle;
 }
 
 @property (nonatomic, assign) CLLocationCoordinate2D coordinate;
 @property (nonatomic, copy) NSString* title;
 @property (nonatomic, copy) NSString* subtitle;
 @property (nonatomic, assign ) NSInteger idUser;
+@property (nonatomic, assign)   CLLocationCoordinate2D coordinate;
+@property (nonatomic, copy)     NSString* title;
+@property (nonatomic, copy)     NSString* subtitle;
+@property (nonatomic, assign)   NSInteger idUser;
 
 @end
